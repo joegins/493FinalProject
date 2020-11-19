2a2624fb9761cd1b12fa316956e2fea617b8dd29
jarocu <jarocu@umich.edu>
2020-11-19 02:14:29 -0500

    drag and drop

---
+++ page.js
@@ -0,0 +1,33 @@
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    clone = ev.target.cloneNode(true);
+    var data = ev.dataTransfer.getData("text");
+    data.innerHTML = ev.dataTransfer.getData("text");
+    ev.target.appendChild(data);
+    let todolist = document.getElementById("app-4").childNodes;
+    for (let i = 0; i < todolist.length; i++) {
+        if (todolist[i].id === data) {
+            dragindex = i;
+        }
+
+    }
+
+
+
+    document.getElementById("app-4").replaceChild(document.getElementById(data), ev.target);
+
+    document.getElementById("app-4").insertBefore(clone, document.getElementById("app-4").childNodes[dragindex]);
+
+}
